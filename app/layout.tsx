ca135a69c0e9e2b7d1fb5b55e48eb6bfc7d1785e
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2024-06-10 15:38:56 +0500

    initial code added

---
+++ app/layout.tsx
@@ -0,0 +1,13 @@
+import { inter } from "./ui/font";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}</body>
+    </html>
+  );
+}
