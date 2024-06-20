fc9eeb8cfd007bd1e852e2b8940dfe4c6e37116a
zaheerbabarkhan <zaheerbaberkhan@gmail.com>
2024-06-20 16:23:37 +0500

    added login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,15 @@
+import { Metadata } from "next";
 import { inter } from "./ui/font";
 import "./ui/global.css"
 
-
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 export default function RootLayout({
   children,
 }: {
